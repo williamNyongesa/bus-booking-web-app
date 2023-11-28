a99dbf1dca3a9a363cddc908ac8654f5533b658c
+ <+>
2023-11-28 08:26:04 +0300

    Signup and Loginin feature

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,19 @@ import React from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import "./App.css";
 import NavbarComp from "./Components/Navbar/Navbar";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
 
 function App() {
+  const navigate = useNavigate();
+  const isLoginSignupRoute = window.location.pathname === "/";
+  const renderNavbar = !isLoginSignupRoute && <NavbarComp />;
+
   return (
     <div className="App">
+      {renderNavbar}
       <NavbarComp />
       <Routes>
-        <Route />
+        <Route exact path="/" element={<LoginSignup />} />
       </Routes>
     </div>
   );
